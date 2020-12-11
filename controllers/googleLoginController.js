2e2c04049e0e8e5c7fa0b3fa60d1380a49fba1af
bùi đình mạnh <buidinhmanh14799@gmail.com>
2020-12-11 12:44:00 +0700

    manh

--- controllers/googleLoginController.js
+++ controllers/googleLoginController.js
@@ -1,45 +1,10 @@
+
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./../config/key');
-
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (profile, done) => {
-
-        // Check if google profile exist.
-        if (profile.id) {
-
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser);
-                    } else {
-                        new User({
-                            googleId: profile.id,
-                            email: profile.emails[0].value,
-                            name: profile.name.familyName + ' ' + profile.name.givenName
-                        })
-                            .save()
-                            .then(user => done(null, user));
-                    }
-                })
-        }
+exports.GoogleLogin = function (req, res) {
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
     })
-);
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => {
-            done(null, user);
-        })
-});
-
-exports
+}
+exports.GoogleCallback = function (req, res) {
+    passport.authenticate('google').then(res.send('oke'))
+}
--- routes/googleRouter.js
+++ routes/googleRouter.js
@@ -1,24 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/userModel')
 passport.use(
-    new GoogleStrategy(
-      {
+    new GoogleStrategy({
         clientID: '457126058033-c4nb6s36lj0lc2c4tgndvap31efuft4f.apps.googleusercontent.com',
         clientSecret: '80BQHBeXdRhXJkoFHj5PK41z',
-        callbackURL: 'google/auth/google/callback'
-      },
-      accessToken => {
-        console.log(accessToken);
-      }
-    )
-  );
-router.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
+        callbackURL: '/google/auth/google/callback'
+    }, (profile, done) => {
+
+        // Check if google profile exist.
+        if (profile.id) {
+
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if (existingUser) {
+                        done(null, existingUser);
+                    } else {
+                        new User({
+                            googleId: profile.id,
+                            email: profile.emails[0].value,
+                            name: profile.name.familyName + ' ' + profile.name.givenName
+                        })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                })
+        }
     })
 );
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
+
+router.get('/auth/google/', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 router.get('/auth/google/callback', passport.authenticate('google'));
 module.exports = router;
